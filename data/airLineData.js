693bf828e2592628d09c04908d84247bf1f36c26
dat.trong <anhtheboy011@gmail.com>
2023-05-07 12:05:59 +0700

    Update airLineData.js

--- data/airLineData.js
+++ data/airLineData.js
@@ -1,7 +1,7 @@
 const airlines = [
     {
         name: "BAMBOO",
-        image: "http://noibaiairport.vn/Assets/images/logo_smis/QH.jpg"
+        image: "https://res.cloudinary.com/devdaz/image/upload/v1683435590/images/BambooAirlway_kvuuqk.jpg"
     },
     {
         name: "Vietnam Airlines",
@@ -9,19 +9,19 @@ const airlines = [
     },
     {
         name: "ASIANA AIRLINES",
-        image: "http://noibaiairport.vn/Assets/images/logo_smis/OZ.jpg"
+        image: "https://res.cloudinary.com/devdaz/image/upload/v1683435598/images/VietnamAirline_yhaubu.jpg"
     },
     {
         name: "Vietjet",
-        image: "http://noibaiairport.vn/Assets/images/logo_smis/VJ.jpg"
+        image: "https://res.cloudinary.com/devdaz/image/upload/v1683435595/images/VietjetAir_qdclby.jpg"
     },
     {
         name: "JetstarPacific",
-        image: "http://noibaiairport.vn/Assets/images/logo_smis/BL.jpg"
+        image: "https://res.cloudinary.com/devdaz/image/upload/v1683435592/images/jestar_pacific_dfivmw.jpg"
     },
     {
         name: "FedEX",
-        image: "http://noibaiairport.vn/Assets/images/logo_smis/FX.jpg"
+        image: "https://res.cloudinary.com/devdaz/image/upload/v1683435594/images/logo-belt_n5bo8y.jpg"
     }
 ];
 
